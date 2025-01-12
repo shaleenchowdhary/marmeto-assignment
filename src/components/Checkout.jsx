229f492f71113e225cc8c97386abe55d3d97fe43
Shaleen Chowdhary <137061973+shaleenchowdhary@users.noreply.github.com>
2025-01-12 16:26:34 +0530

    fixed typo in checkout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,7 +2,7 @@ const Checkout = () => {
   return (
     <div className="h-[70%] flex justify-center items-center flex-col">
       <h2 className="text-xl">Checkout</h2>
-      <h2 className="text-xl">{`//Details realted to order here...`}</h2>
+      <h2 className="text-xl">{`//Details related to order here...`}</h2>
     </div>
   );
 };
